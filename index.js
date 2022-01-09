8f962eb2e8775885060f0933ce045bd88af0b916
Karthikeyan Y <karthikeyan.y@BLRETV-C02F2NK7.hgu_lan>
2022-01-09 09:18:56 +0530

    adds dynamic port for heroku deployment

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function sendNotifications(subscriptions) {
 
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, '/client')));
 app.use(bodyParser.json());
